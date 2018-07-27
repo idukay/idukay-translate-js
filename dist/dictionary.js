449c5865e8c5821b725ec5ba359226f32686f3a9
Jonathan Tinajero <jnth_pt@hotmail.com>
2018-07-27 13:19:42 -0500

    Added dist folder

--- dist/dictionary.js
+++ dist/dictionary.js
@@ -4,11 +4,12 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 
 (function () {
   var dictionary = function dictionary(_ref) {
-    var _ref$id = _ref.id,
-        id = _ref$id === undefined ? JSON.parse(localStorage.getItem('idukayTranslateJS')).id : _ref$id,
+    var id = _ref.id,
         dictionaries = _ref.dictionaries;
 
+    var storage = JSON.parse(localStorage.getItem('idukayTranslateJS')) || {};
     var isArray = Array.isArray(dictionaries[0].values);
+    var dictionaryId = id || storage.id;
 
     if (isArray) {
       dictionaries.forEach(function (dictionary) {
@@ -25,24 +26,24 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
     }
 
     var dictionary = dictionaries.find(function (dictionary) {
-      return dictionary.id === id;
+      return dictionary.id === dictionaryId;
     });
 
-    dictionary = dictionary || { values: [] };
+    dictionary = Object.assign({ values: [] }, dictionary);
     localStorage.setItem('idukayTranslateJS', JSON.stringify(dictionary));
   };
 
   var t = function t(key, value) {
-    return JSON.parse(localStorage.getItem('idukayTranslateJS')).values[key] || value;
+    return JSON.parse(localStorage.getItem('idukayTranslateJS')).values[key.toLowerCase()] || value;
   };
 
   var root = (typeof self === 'undefined' ? 'undefined' : _typeof(self)) == 'object' && self.self === self && self || (typeof global === 'undefined' ? 'undefined' : _typeof(global)) == 'object' && global.global === global && global || this || {};
 
   if (typeof exports !== 'undefined' && !exports.nodeType) {
-    exports.dictionary = dictionary;
+    exports.translateJs = dictionary;
     exports.t = t;
   } else {
-    root.translateJs = instance;
+    root.translateJs = dictionary;
     root.t = t;
   }
 })();
--- dist/specs/dictionary.spec.js
+++ dist/specs/dictionary.spec.js
@@ -22,7 +22,7 @@ describe('Dictionary', function () {
 
       it('should instance dictionary.t and return correct translation', function () {
         expect((0, _dictionary.t)('key1', 'enOtherText1')).toBe('esTranslateKey1');
-        expect((0, _dictionary.t)('key2', 'enOtherText2')).toBe('esTranslateKey2');
+        expect((0, _dictionary.t)('kEY2', 'enOtherText2')).toBe('esTranslateKey2');
       });
     });
 
@@ -62,7 +62,7 @@ describe('Dictionary', function () {
         var newDictionaries = [{
           id: 'en-us',
           label: 'EN',
-          values: { newKey: 'newKeyValue' }
+          values: { newkey: 'newKeyValue' }
         }];
 
         localStorage.setItem('idukayTranslateJS', JSON.stringify({ id: 'en-us' }));
@@ -71,6 +71,20 @@ describe('Dictionary', function () {
 
       it('should instance t and return new value', function () {
         expect((0, _dictionary.t)('newKey', 'value')).toBe('newKeyValue');
+      });
+    });
+
+    describe('when not has localStorage', function () {
+      beforeEach(function () {
+        var newDictionaries = [{
+          id: undefined
+        }];
+
+        (0, _dictionary.dictionary)({ dictionaries: newDictionaries });
+      });
+
+      it('should instance t and return value', function () {
+        expect((0, _dictionary.t)('key', 'value')).toBe('value');
       });
     });
   });
